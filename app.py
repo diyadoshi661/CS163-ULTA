6f18f589e2df27a9ab55cab6979666f71d19ab18
Diya Doshi <diyadoshi@Diyas-MacBook-Air.local>
2025-04-28 15:05:10 -0700

    changes for gcloud

--- app.py
+++ app.py
@@ -79,5 +79,8 @@ def update_to_main_layout(n):
         return full_layout()
     return dash.no_update
 
+
+server = app.server
+
 if __name__ == "__main__":
-    app.run(debug=True)# use_reloader=False
+    app.run(debug=False, use_reloader=False)# use_reloader=False
